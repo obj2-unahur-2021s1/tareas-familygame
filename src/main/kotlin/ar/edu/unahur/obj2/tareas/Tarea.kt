77f0dad96866a638b65f96c8f92aee5cb341fdec
CarlosAttanasio <attanasiocarlos@gmail.com>
2021-06-06 20:20:32 -0300

    agregado nomina de empleados y costo de tareas

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -3,7 +3,8 @@ package ar.edu.unahur.obj2.tareas
 interface Tarea {
 
     fun horasNecesarias() : Double
-
+    fun costoTarea() : Double
+    fun nominaEmpleados(): MutableList<Empleado>
 }
 
 class Simple(private val horasEstimadas : Double,
@@ -13,17 +14,25 @@ class Simple(private val horasEstimadas : Double,
     : Tarea {
 
     override fun horasNecesarias() = this.horasEstimadas / empleadosAsignados.size
-
-
+    override fun costoTarea() = this.costoInfraestructura + empleadosAsignados.sumByDouble { it.calcularSalario(horasEstimadas)} + responsable.calcularSalario(horasEstimadas)
+    override fun nominaEmpleados(): MutableList<Empleado> {
+        var nominaEmpleados = this.empleadosAsignados
+        nominaEmpleados.add(responsable)
+        return nominaEmpleados
+    }
 }
 
-class Integracion(responsable : Empleado) : Tarea{
+class Integracion(private val responsable : Empleado) : Tarea{
 
     private var tareas = mutableListOf<Tarea>()
 
     override fun horasNecesarias() = tareas.sumByDouble { it.horasNecesarias() } + (tareas.sumByDouble { it.horasNecesarias() / 8 })
     private fun costoSubTareas() = tareas.sumByDouble { it.costoTarea() }
     private fun bonus() = costoSubTareas() * 0.03
-
-
+    override fun costoTarea() = costoSubTareas() + bonus()
+    override fun nominaEmpleados(): MutableList<Empleado> {
+        var nominaEmpleados = tareas.flatMap { it.nominaEmpleados() }.toMutableList()
+        nominaEmpleados.add(responsable)
+        return nominaEmpleados
+    }
 }
